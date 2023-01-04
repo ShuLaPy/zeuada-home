45c644aecc5d9c4c34685ce6a0487f8fafbcfae3
Shubham Lad <shubham.lad@softway.com>
2023-01-04 23:52:53 +0530

    Initial folder structure

---
+++ src/pages/newtab/components/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "@pages/newtab/components/Home/Home.css";
+import "@pages/newtab/components/Home/Home.scss";
+import BackgroundImage from "../BackgroundImage/BackgroundImage";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <BackgroundImage />
+    </div>
+  );
+};
+
+export default Home;
